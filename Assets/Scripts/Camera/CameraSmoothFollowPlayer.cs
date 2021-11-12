35513c54dee41f4a5f96fa59036b70527b0c785d
zgurovski <zgurovski00@gmail.com>
2021-11-12 19:41:42 +0200

    Improved follow camera script and added more settings

--- Assets/Scripts/Camera/CameraSmoothFollowPlayer.cs
+++ Assets/Scripts/Camera/CameraSmoothFollowPlayer.cs
@@ -1,24 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSmoothFollowPlayer : MonoBehaviour
 {
 
+    public bool FocusAtStart = true;
     public float dampTime = 0.15f;
-    private Vector3 velocity = Vector3.zero;
     public Transform target;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        if (FocusAtStart)
+        {
+            Vector3 targetPos = target.position;
+            targetPos.z = transform.position.z;
+            transform.position = targetPos;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (target)
-        {
-            Vector3 point = Camera.main.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-        }
+        if (target == null) return;
 
+        Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+        Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        Vector3 destination = transform.position + delta;
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
     }
 }

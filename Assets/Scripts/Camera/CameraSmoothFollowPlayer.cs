3fe5d66929736f4817bef8c245cc924e3079eaf2
zgurovski <zgurovski00@gmail.com>
2021-11-15 21:05:15 +0200

    added basic camera restrictions

--- Assets/Scripts/Camera/CameraSmoothFollowPlayer.cs
+++ Assets/Scripts/Camera/CameraSmoothFollowPlayer.cs
@@ -7,7 +7,14 @@ public class CameraSmoothFollowPlayer : MonoBehaviour
     public bool FocusAtStart = true;
     public float dampTime = 0.15f;
     public Transform target;
+    public Transform background;
+    
     private Vector3 velocity = Vector3.zero;
+    private Vector2 backgroundBounds;
+    //TODO: change type to Vector2
+    private Bounds cameraBounds;
+    private float offsetX = 0.5f;
+    private float offsetY = 0.5f;
 
     private void Start()
     {
@@ -17,6 +24,14 @@ public class CameraSmoothFollowPlayer : MonoBehaviour
             targetPos.z = transform.position.z;
             transform.position = targetPos;
         }
+
+        // Get tge background bounds
+        backgroundBounds = new Vector2(
+            background.GetComponent<SpriteRenderer>().bounds.size.x / 2,
+            background.GetComponent<SpriteRenderer>().bounds.size.y / 2);
+
+        // Get the camera bounds
+        cameraBounds = OrthographicBounds(Camera.main);
     }
 
 
@@ -28,6 +43,37 @@ public class CameraSmoothFollowPlayer : MonoBehaviour
         Vector3 point = Camera.main.WorldToViewportPoint(target.position);
         Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
         Vector3 destination = transform.position + delta;
-        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        Vector3 result = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+        if (CameraIsAtTheEdge()) {
+            return;
+        }
+
+        transform.position = result;
+    }
+
+    private bool CameraIsAtTheEdge()
+    {
+        Debug.Log(transform.position.y + " => " + cameraBounds.size.y + " => " + backgroundBounds.y);
+        
+        // We have reached the top edge of the background and we stop
+        if (transform.position.y + (cameraBounds.size.y / 2) > backgroundBounds.y - offsetX)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the ortigraphic bounds of the camera
+     */
+    private Bounds OrthographicBounds(Camera camera)
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
     }
 }
